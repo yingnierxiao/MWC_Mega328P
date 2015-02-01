4e3024daf813db382564e142a49f5d1d693301f8
skypupChina <skypup@qq.com>
2015-02-02 02:48:39 +0800

    .ino 注释重写。 config.h 的 PPM 通道定义改为 Futaba 兼容格式。
    Signed-off-by: skypupChina <skypup@qq.com>

--- MWC_Mega328P.ino
+++ MWC_Mega328P.ino
@@ -1,14 +1,72 @@
 
 /*
- * Welcome to MultiWii.
- *
- * If you see this message, chances are you are using the Arduino IDE. That is ok.
- * To get the MultiWii program configured for your copter, you must switch to the tab named 'config.h'.
- * Maybe that tab is not visible in the list at the top, then you must use the drop down list at the right
- * to access that tab. In that tab you must enable your baord or sensors and optionally various features.
- * For more info go to http://www.multiwii.com/wiki/index.php?title=Main_Page
- *
- * Have fun, and do not forget MultiWii is made possible and brought to you under the GPL License.
- *
+ * MWC Mini 端口定义 (2015.01.31 Mega328P-AU)
+ * 
+ * 接收器
+ * GND
+ * +5V
+ * IN3 / PPM    D2   PD2
+ * IN1          D4   PD4
+ * IN2          D5   PD5
+ * IN4          D6   PD6            
+ * IN5          D7   PD7
+
+ * PWM输出
+ * O8           A2   PC2
+ * O7           A1   PC1
+ * O6           A0   PC0
+ * O4           D3   PD3
+ * O1           D12  PB4 / MISO
+ * O5           D11  PB3 / MOSI
+ * O2           D10  PB2
+ * O3           D9   PB1
+ * 
+ * IO口
+ * BATT         A3   PC3
+ * LED          D13  PB5 / SCK
+ * +5V
+ * ALARM        D8   PB0
+ * GND
+ * 
+ * TTL串口
+ * +5V
+ * GND
+ * RX
+ * TX
+ * RESETMWC Mini 端口定义
+ * 
+ * 接收器
+ * GND
+ * +5V
+ * IN3 / PPM    D2   PD2
+ * IN1          D4   PD4
+ * IN2          D5   PD5
+ * IN4          D6   PD6            
+ * IN5          D7   PD7
+ * 
+ * PWM输出
+ * O8           A2   PC2
+ * O7           A1   PC1
+ * O6           A0   PC0
+ * O4           D3   PD3
+ * O1           D12  PB4 / MISO
+ * O5           D11  PB3 / MOSI
+ * O2           D10  PB2
+ * O3           D9   PB1
+ * 
+ * IO口
+ * BATT         A3   PC3
+ * LED          D13  PB5 / SCK
+ * +5V
+ * ALARM        D8   PB0
+ * GND
+ * 
+ * TTL串口
+ * +5V
+ * GND
+ * RX
+ * TX
+ * RESET
+ * 
  */
 
